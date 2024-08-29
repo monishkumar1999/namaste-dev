c593f86c1a088acc214c2606e3fdffe2561e25df
monish kumar <monishkumarapj@gmail.ocm>
2024-08-29 07:36:05 +0530

    29 aguest about jsx

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import ReactDOM from "react-dom/client"; 
 
-const heading = React.createElement("div", {}, 'React, I am here');
+//  const heading = React.createElement("div", {}, 'React, I am here');
+
+
+ const jsx=<h1>hai</h1>; //jsx syntax
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(jsx);
