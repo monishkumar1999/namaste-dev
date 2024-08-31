c4bb86f3f7a5efc549b36c1c7f029e11fe4a9309
monish kumar <monishkumarapj@gmail.ocm>
2024-08-31 06:27:59 +0530

    31

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"; 
 
 
-const d =<h1>i am from React</h1>;  //jsx to react element -> react object
+const d =(<h1 className="head">i am from React</h1>);  //jsx to react element -> react object
 
 const root =ReactDOM.createRoot(document.getElementById('root'));
 
