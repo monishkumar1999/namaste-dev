903154e2a14a7d1ac2353d6dca18ad2d2a962455
monish kumar <monishkumarapj@gmail.ocm>
2024-09-01 20:43:42 +0530

    sunday swiggy

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
 
-import ReactDOM from "react-dom/client"; 
+const Header = () => (
+  <div className="header row d-flex justify-content-between border border-primary">
+    <div className="logo col-sm-6">
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/025/379/860/non_2x/food-bag-for-restaurant-delivery-service-logo-design-concept-illustration-symbol-icon-vector.jpg"
+        className=""
+        alt="Logo"
+        width={90}
+      />
+    </div>
 
+    <div className="nav_item col-sm-6 ">
+      <ul className="d-flex justify-content-end list-unstyled p-4 h5">
+        <li className="m-2">Home</li>
+        <li className="m-2">About</li>
+        <li className="m-2">Cart</li>
+        <li className="m-2">Contact Us</li>
+      </ul>
+    </div>
+  </div>
+);
 
-const d =(<h1 className="head">i am from React</h1>);  //jsx to react element -> react object
+const Body = () => (
+  <div className="body">
+    <div className="search">search</div>
+  </div>
+);
+const Appcomponent = () => {
+  return (
+    <div className="app">
+      <Header />
 
-const root =ReactDOM.createRoot(document.getElementById('root'));
+      <Body/>
+    </div>
+  );
+};
 
-root.render(d);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Appcomponent />);
