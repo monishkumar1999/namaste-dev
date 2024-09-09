120694d7e1a4580c614e2e8c56a298076da18023
monish kumar <monishkumarapj@gmail.ocm>
2024-09-09 07:58:53 +0530

    routs

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,20 +1,14 @@
 import { LOGO_URL } from "../utill/constant.js";
 import { useState } from "react";
 
-
 export const Header = () => {
-  const [btnName,btnchange]=useState("login");
-return(
-
+  const [btnName, btnchange] = useState("login");
+  return (
     <div className="header row d-flex justify-content-between border border-primary">
       <div className="logo col-sm-6">
-        <img
-          src={LOGO_URL}
-          alt="Logo"
-          width={90}
-        />
+        <img src={LOGO_URL} alt="Logo" width={90} />
       </div>
-  
+
       <div className="nav_item col-sm-6">
         <ul className="d-flex justify-content-end list-unstyled p-4 h5">
           <li className="m-2">Home</li>
@@ -22,14 +16,17 @@ return(
           <li className="m-2">Cart</li>
           <li className="m-2">Contact us</li>
 
-          <button className="btn btn-primary" onClick={()=>{
-          btnName=="login"? btnchange("logout") :btnchange("login")
-          
-          }}
-          >{btnName}</button>
+          <button
+            className="btn btn-primary"
+            onClick={() => {
+              btnName == "login" ? btnchange("logout") : btnchange("login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
     </div>
   );
-}
-  export default Header;
+};
+export default Header;
