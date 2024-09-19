99cb4202a1f54aa8c78319f3528becdd38cd51d4
monish kumar <monishkumarapj@gmail.ocm>
2024-09-19 08:38:33 +0530

    19

--- src/component/RestraruntCatogry.js
+++ src/component/RestraruntCatogry.js
@@ -1,20 +1,26 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
-const RestaruantCatogry=(item)=>{
-    // console.log(item)
-    return (
-       <div className="bg-gray-100 p-4 w-6/12 m-auto shadow-lg my-4" onClick={()=>{
-        console.log("hai")
-       }}>
-        <div className=" flex justify-between" >
-             {/* // {accoridan header} */}
-            <span className="font-bold">{item.item.title} ({item.item.itemCards.length})</span>
-            <span >&#8628;</span>
-             {/* {accoridan body} */}
-            
-        </div>
-        <ItemList items={item.item.itemCards}/>
-        </div>
-    )
-}
+const RestaruantCatogry = (item) => {
+  const [showitem, setShowitem] = useState(false);
+  return (
+    <div
+      className="bg-gray-100 p-4 w-6/12 m-auto shadow-lg my-4"
+      onClick={() => {
+        // console.log(item.item.title)
+        showitem == false ? setShowitem(true) : setShowitem(false);
+      }}
+    >
+      <div className=" flex justify-between cursor-pointer">
+        {/* // {accoridan header} */}
+        <span className="font-bold">
+          {item.item.title} ({item.item.itemCards.length})
+        </span>
+        <span>&#8628;</span>
+        {/* {accoridan body} */}
+      </div>
+      {showitem && <ItemList items={item.item.itemCards} />}
+    </div>
+  );
+};
 
 export default RestaruantCatogry;
